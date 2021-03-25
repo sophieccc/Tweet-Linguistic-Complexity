010753ebc14ec44c72b93f381d303a4bb4318e6f
Sophie Crowley <crowleso@tcd.ie>
2021-03-25 16:36:21 +0000

    Initial commit

---
+++ recent_search.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import json
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+
+
+def auth():
+    return os.environ.get("BEARER_TOKEN")
+
+
+def create_url():
+    query = "context:10.799022225751871488"
+    max_results = "max_results=10";
+    tweet_fields = "tweet.fields=author_id"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}".format(
+        query, tweet_fields, max_results
+    )
+    return url
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    bearer_token = auth()
+    url = create_url()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
