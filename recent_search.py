4c851a8efd904569d7e7144018ce3328e28cbac3
Sophie Crowley <crowleso@tcd.ie>
2021-03-26 01:09:29 +0000

    Writing tweets to file

--- recent_search.py
+++ recent_search.py
@@ -10,8 +10,7 @@ def auth():
     return os.environ.get("BEARER_TOKEN")
 
 # This decides what tweets we are retrieving.
-def create_url(token):
-    query = "context:10.799022225751871488"
+def create_url(token, query):
     max_results = "max_results=10"
     tweet_fields = "tweet.fields=author_id"
     next_token = ""
@@ -30,31 +29,37 @@ def create_headers(bearer_token):
 # Retrieves the tweets and metadata.ß
 def connect_to_endpoint(url, headers):
     response = requests.request("GET", url, headers=headers)
-    print(response.status_code)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
 
 
 def main():
+    tweets = []
     bearer_token = auth()
     headers = create_headers(bearer_token)
     valid_token = True
     token = None
     index = 0
-    # When actually retrieving data, remove index and set max_results = 100.
-    while valid_token and index < 2:
-        url = create_url(token)
-        json_response = connect_to_endpoint(url, headers)
-        data = json_response["data"]
-        meta = json_response["meta"]
-        if "next_token" in meta:
-            token = meta["next_token"]
-        else:
-            valid_token = False
-        print(json.dumps(data, indent=4, sort_keys=True))
-        print(json.dumps(meta, indent=4, sort_keys=True))
-        index +=1
+    # Will have to do 'contexts' in terms of Entity annotations, 
+    # or just in terms of entities in general.
+    queries = ["entity:Donald Trump lang:en -is:retweet"]
+    for query in queries:
+        # When actually retrieving data, remove index and set max_results = 100.
+        while valid_token and index < 2:
+            url = create_url(token, query)
+            json_response = connect_to_endpoint(url, headers)
+            data = json_response["data"]
+            meta = json_response["meta"]
+            if "next_token" in meta:
+                token = meta["next_token"]
+            else:
+                valid_token = False
+            for tweet in data:
+                tweets.append(tweet)
+            index +=1
+        with open('tweets.json', 'a') as f:
+            json.dump(tweets,f)
 
 
 
