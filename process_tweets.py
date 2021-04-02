b597921c0be45d7aff6f748d8ef71e035f30810f
Sophie Crowley <crowleso@tcd.ie>
2021-04-03 00:58:44 +0100

    Removing links

--- process_tweets.py
+++ process_tweets.py
@@ -1,36 +1,40 @@
 import json
 import re
 
-def write_unique_tweets():
+def write_unique_tweets(data):
     unique_tweets = {}
-    with open('sports_pre.json') as json_file:
-        data = json.load(json_file)
-        for key, value in data.items():
-            unique_tweets[key] = value
-
-    with open('sports.json', 'a') as f:
-        json.dump(unique_tweets,f)
+    for key, value in data.items():
+        unique_tweets[key] = value
+    return unique_tweets
 
 # This function removes any emojis and replaces usernames  
 # with a person's name ('Sophie'). If there are multiple usernames
 # in a row, they will all be replaced by one name.
-def remove_usernames_and_emojis():
+def remove_usernames_and_emojis(data):
     tweets = {}
-    with open('politics_pre.json') as json_file:
-        data = json.load(json_file)
-        for key, value in data.items():
-            no_user = " ".join(value.split())
-            no_user2 = re.sub('@[a-zA-Z0-9_]*', '  ', no_user)
-            no_user3 = re.sub(' {2,}', ' Sophie ', no_user2)
-            no_emojis = no_user3.encode('ascii', 'ignore').decode('ascii')
-            tweets[key] = " ".join(no_emojis.split())
+    for key, value in data.items():
+        no_user = " ".join(value.split())
+        no_user2 = re.sub('@[a-zA-Z0-9_]*', '  ', no_user)
+        no_user3 = re.sub(' {2,}', ' Sophie ', no_user2)
+        no_emojis = no_user3.encode('ascii', 'ignore').decode('ascii')
+        tweets[key] = " ".join(no_emojis.split())
+    return tweets
 
-    with open('politics.json', 'a') as f:
-        json.dump(tweets,f)
+def remove_links(data):
+    tweets = {}
+    for key, value in data:
+        replaced = re.sub('https://t.co/[a-zA-Z0-9]*', '  ', value)
+        tweets[key] = re.sub(' {2,}', ' ', replaced)
+    return tweets
 
 def main():
-    #write_unique_tweets()
-    remove_usernames_and_emojis()
+    with open('sports_pre.json') as json_file:
+        data = json.load(json_file)
+        #write_unique_tweets()
+        tweets = remove_usernames_and_emojis(data)
+        tweets2 = remove_links(tweets.items())
+        with open('sports.json', 'a') as f:
+            json.dump(tweets2,f)
 
 if __name__ == "__main__":
     main()
